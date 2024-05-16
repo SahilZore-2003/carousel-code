28c996bd81aef46375313968c42c8e3d32f8ee8c
SahilZore <106907644+SahilZore-2003@users.noreply.github.com>
2024-05-16 12:35:52 +0530

    Create main.jsx

---
+++ main.jsx
@@ -0,0 +1,27 @@
+ const featuredSectionData = [
+        {
+            image: "https://media.istockphoto.com/id/2012747478/photo/businessman-computer-or-programming-code-in-night-office-for-software-development.jpg?s=2048x2048&w=is&k=20&c=hpdMAKyO26EFAJ-xB_w0SWCIshT0LN5OF3Kp4eVEWsk=",
+            description:
+                '<h1 class="text-white text-center font-bold text-4xl">Use essential <span class=" font-semibold">skill</span> tool</h1><div class="flex items-start flex-col mt-4 gap-4" ><p class="grow text-white text-center text-sm sm:text-lg">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Rerum, reprehenderit?</p><button class="bg-white rounded-xl md:mt-4 py-2 px-4 whitespace-nowrap font-bold mx-auto">Apply Now</button></div > ',
+        },
+        {
+            image: "https://plus.unsplash.com/premium_photo-1663075847012-c781e0d194ce?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8aG9yaXpvbnRhbCUyMHdhbGxwYXBlciUyMGNvZGluZ3xlbnwwfHwwfHx8MA%3D%3D",
+            description:
+                '<h1 class="text-white text-center font-bold text-4xl">Use essential <span class=" font-semibold">skill</span> tool</h1><div class="flex items-start flex-col mt-4 gap-4" ><p class="grow text-white text-center text-sm sm:text-lg">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Rerum, reprehenderit?</p><button class="bg-white rounded-xl md:mt-4 py-2 px-4 whitespace-nowrap font-bold mx-auto">Apply Now</button></div > ',
+        },
+        {
+            image: "https://plus.unsplash.com/premium_photo-1661877737564-3dfd7282efcb?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OXx8aG9yaXpvbnRhbCUyMHdhbGxwYXBlciUyMGNvZGluZ3xlbnwwfHwwfHx8MA%3D%3D",
+            description:
+                '<h1 class="text-white text-center font-bold text-4xl">Use essential <span class=" font-semibold">skill</span> tool</h1><div class="flex items-start flex-col mt-4 gap-4" ><p class="grow text-white text-center text-sm sm:text-lg">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Rerum, reprehenderit?</p><button class="bg-white rounded-xl md:mt-4 py-2 px-4 whitespace-nowrap font-bold mx-auto">Apply Now</button></div > ',
+        },
+        {
+            image: "https://media.istockphoto.com/id/1329913812/photo/banking-icon-concept.webp?b=1&s=170667a&w=0&k=20&c=dstcrqIfWnIum4pulyoDSc2NAtaFD4y0nvmaERGszC4=",
+            description:
+                '<h1 class="text-white text-center font-bold text-4xl">Use essential <span class=" font-semibold">skill</span> tool</h1><div class="flex items-start flex-col mt-4 gap-4" ><p class="grow text-white text-center text-sm sm:text-lg">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Rerum, reprehenderit?</p><button class="bg-white rounded-xl md:mt-4 py-2 px-4 whitespace-nowrap font-bold mx-auto">Apply Now</button></div > ',
+        }
+    ]
+
+
+<div className="my-12 bg-gradient-to-r from-gradient-first from-30% via-gradient-second 60% to-gradient-third to-80%  py-4">
+                <FeaturedTestimonials data={featuredSectionData} template={"-five"} />
+            </div>
